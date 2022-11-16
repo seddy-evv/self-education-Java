4ef4802bda6d1e7358919df2e6331dc5f7aa6b42
Anatol <seddboom@gmail.com>
2022-11-16 14:54:30 +0300

    add Three sum class

---
+++ src/main/java/ThreeSum.java
@@ -0,0 +1,24 @@
+public class ThreeSum {
+    public static void printTriples(int[] a)
+    {}
+
+    public static int countTriples(int [] a)
+    {
+        int n = a.length;
+        int count = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i+1; j < n; j++)
+                for (int k = j+1; k < n; k++)
+                    if (a[i] + a[j] + a[k] == 0)
+                        count++;
+         return count;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] a = StdIn.readAllInts();
+        int count = countTriples(a);
+        StdOut.println(count);
+        if (count < 10) printTriples(a);
+    }
+}
