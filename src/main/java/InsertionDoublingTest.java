294384dae99a280baa54324f207a8dd7b87de9ee
Anatol <seddboom@gmail.com>
2022-12-08 13:43:27 +0300

    fix Insertion doubling test class

--- src/main/java/InsertionDoublingTest.java
+++ src/main/java/InsertionDoublingTest.java
@@ -1,3 +1,11 @@
+/**
+ * The timeTrials() method performs Insertion.sort() on arrays of random doubles. The first argument
+ * n is the length of the array; the second argument to trials specifies the number of trials.
+ * Increasing the number of trials improves the accuracy of the results because it eliminates system-specific
+ * effects and the actual insertion sort time depends on the input.
+ **/
+
+
 public class InsertionDoublingTest {
     public static double timeTrials(int trials, int n)
     {
@@ -6,7 +14,7 @@ public class InsertionDoublingTest {
         for (int t = 0; t < trials; t++)
         {
             for (int i = 0; i < n; i++)
-                a[i] = StdRandom.uniform(0.0, 1.0);
+                a[i] = StdRandom.uniformDouble(0.0, 1.0);
             Stopwatch timer = new Stopwatch();
             Insertion.sort(a);
             total += timer.elapsedTime();
@@ -15,7 +23,9 @@ public class InsertionDoublingTest {
     }
     public static void main(String[] args)
     {
-        int trials = Integer.parseInt(args[0]);
+//        int trials = Integer.parseInt(args[0]);
+        StdOut.println("Enter the number of trials: ");
+        int trials = StdIn.readInt();
         for (int n = 1024; true; n += n)
         {
             double prev = timeTrials(trials, n/2);
