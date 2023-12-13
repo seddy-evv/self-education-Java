52ffe02350d472af4233bad0e128de1799594e92
Anatol <seddboom@gmail.com>
2023-12-13 23:00:16 +0300

    update InputValue class

--- src/main/java/examples/InputValue.java
+++ src/main/java/examples/InputValue.java
@@ -3,6 +3,7 @@ package examples;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * A simple example of getting values from the console
@@ -13,13 +14,19 @@ public class InputValue
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter String");
+        System.out.print("Enter String:");
         String s = br.readLine();
         System.out.print("Enter Integer:");
+        int i = 0;
         try {
-            int i = Integer.parseInt(br.readLine());
+            i = Integer.parseInt(br.readLine());
         } catch(NumberFormatException nfe) {
             System.err.println("Invalid Format!");
         }
+        System.out.println("String - " + s + "\n" + "Integer - " + i);
+//        Another approach
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
