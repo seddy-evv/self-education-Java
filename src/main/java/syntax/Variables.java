93c93027cf227764137567a590fb92d069db53c2
Anatol <seddboom@gmail.com>
2023-12-14 23:29:52 +0300

    add Variables class

---
+++ src/main/java/syntax/Variables.java
@@ -0,0 +1,26 @@
+package syntax;
+
+public class Variables {
+
+    public static void main(String[] args)
+    {
+        //Variables in the camelCase
+        int clientAge;
+        clientAge = 25;
+        //OR
+        int customerAge = 25;
+        //Primitive data types
+        byte byteMax = 127;
+        short shortMax = 32767;
+        int intMax = 2147483647;
+        long longMax = 9223372036854775807L;
+        float salary = 1232f;
+        double biggerSalary = 900717887.9;
+        char initial = 'A';
+        boolean isStudent = true;
+        //Another data types
+        String name = "John";
+        String[] names = {"John", "Doe", "Jane"};
+    }
+
+}
