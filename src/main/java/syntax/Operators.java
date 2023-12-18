c580a6a36f72a88563436ede03d5566a8de06d46
Anatol <seddboom@gmail.com>
2023-12-18 18:42:05 +0300

    add Opeartors class

---
+++ src/main/java/syntax/Operators.java
@@ -0,0 +1,52 @@
+package syntax;
+
+public class Operators {
+
+    public static void main(String[] args)
+    {
+        int a = 25;
+        int b = 15;
+        double c = 25;
+        double d = 15;
+        System.out.print("a + b = ");
+        System.out.println(a + b);
+        System.out.print("a - b = ");
+        System.out.println(a - b);
+        System.out.print("a * b = ");
+        System.out.println(a * b);
+        System.out.print("a / b = ");
+        System.out.println(a / b);
+        System.out.print("c / d = ");
+        System.out.println(c / d);
+        System.out.print("a % b = ");
+        System.out.println(a % b);
+        System.out.print("a == b = ");
+        System.out.println(a == b);
+        System.out.print("a != b = ");
+        System.out.println(a != b);
+        System.out.print("a > b = ");
+        System.out.println(a > b);
+        System.out.print("a < b = ");
+        System.out.println(a < b);
+        System.out.print("a >= b = ");
+        System.out.println(a >= b);
+        System.out.print("a <= b = ");
+        System.out.println(a <= b);
+        System.out.print("a += b = ");
+        a += b;
+        System.out.println(a);
+        System.out.print("a -= b = ");
+        a -= b;
+        System.out.println(a);
+        System.out.print("a *= b = ");
+        a *= b;
+        System.out.println(a);
+        System.out.print("a /= b = ");
+        a /= b;
+        System.out.println(a);
+        System.out.print("a %= b = ");
+        a %= b;
+        System.out.println(a);
+
+    }
+}
