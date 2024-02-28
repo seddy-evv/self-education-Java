8ed94234183add20fc86e278c2fe847d9c80d1f1
Anatol <seddboom@gmail.com>
2024-02-28 21:38:46 +0300

    add Calculator class

---
+++ src/main/java/syntax/Calculator.java
@@ -0,0 +1,31 @@
+package syntax;
+/**
+ * This class represents a simple calculator.
+ *
+ * @author Anatol
+ * @version 1.0
+ */
+public class Calculator {
+
+    /**
+     * Adds two integers.
+     *
+     * @param a the first operand
+     * @param b the second operand
+     * @return the sum of a and b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Subtracts two integers.
+     *
+     * @param a the first operand
+     * @param b the second operand
+     * @return the difference between a and b
+     */
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
