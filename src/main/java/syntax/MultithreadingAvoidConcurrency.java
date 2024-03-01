3f92ed62ec3a72bc8ec0a3b7f50a1191def78ff4
Anatol <seddboom@gmail.com>
2024-03-01 15:33:31 +0300

    add thread classes

---
+++ src/main/java/syntax/MultithreadingAvoidConcurrency.java
@@ -0,0 +1,21 @@
+package syntax;
+
+public class MultithreadingAvoidConcurrency extends Thread {
+    public static int amount = 0;
+
+    public static void main(String[] args) {
+        MultithreadingAvoidConcurrency thread = new MultithreadingAvoidConcurrency();
+        thread.start();
+        // Wait for the thread to finish
+        while(thread.isAlive()) {
+            System.out.println("Waiting...");
+        }
+        // Update amount and print its value
+        System.out.println("Main: " + amount);
+        amount++;
+        System.out.println("Main: " + amount);
+    }
+    public void run() {
+        amount++;
+    }
+}
